7ad1528174cdf736a7e236dd05da94a790a786b5
mdamicoml <martin.damico@mercadolibre.com>
2020-04-01 10:53:42 -0300

    fixed TextFieldType

--- LibraryComponents/Classes/AndesTextField/Type/AndesTextFieldType.swift
+++ LibraryComponents/Classes/AndesTextField/Type/AndesTextFieldType.swift
@@ -8,22 +8,11 @@
 import Foundation
 
 /// Used to define type of textfield
-@objc public enum AndesTextFieldType: Int, LosslessStringConvertible {
+@objc public enum AndesTextFieldType: Int, AndesEnumStringConvertible {
     case simple
     case textArea
 
     internal static var keys: [String] {
         ["SIMPLE", "TEXT_AREA"]
-    }
-
-    public init?(_ description: String) {
-        guard let rawValue = AndesTextFieldType.keys.firstIndex(of: description.uppercased()) else {
-            return nil
-        }
-        self.init(rawValue: rawValue)
-    }
-
-    public var description: String {
-        return AndesTextFieldType.keys[self.rawValue]
     }
 }
