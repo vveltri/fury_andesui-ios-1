adba2435a561862f5b49fc89c948359c4ff53afa
Nicolas Rostan <nicolas.rostan@mercadolibre.con>
2020-04-03 09:23:56 -0300

    remove msg changes

--- Example/AndesUI/Messages/Views/MessageViewController.swift
+++ Example/AndesUI/Messages/Views/MessageViewController.swift
@@ -218,21 +218,6 @@ extension AndesMessageType {
             return "Warning"
         }
     }
-
-    func fromString(_ str: String) -> AndesMessageType {
-        switch str.lowercased() {
-        case "neutral":
-            return .neutral
-        case "success":
-            return .success
-        case "error":
-            return .error
-        case "warning":
-            return .warning
-        default:
-            return .neutral
-        }
-    }
 }
 
 extension AndesMessageHierarchy {
