8e838d9bb4d6fb1f4dd34b5277b474f3ead4e924
Nicolas Rostan <nicolas.rostan@mercadolibre.con>
2020-04-15 15:27:23 -0300

    testapp with objc

--- Example/AndesUI/TextFields/Views/TextFieldObjCViewController.h
+++ Example/AndesUI/TextFields/Views/TextFieldObjCViewController.h
@@ -7,10 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AndesUI-Swift.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TextFieldObjCViewController : UIViewController
+@interface TextFieldObjCViewController : UIViewController <AndesTextFieldDelegate>
 
 @end
 
