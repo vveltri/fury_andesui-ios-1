3e27afdfb4fc058ecd5ab399db323d3311895b4c
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2020-06-26 14:50:48 -0300

    Update test

--- Example/Tests/AndesCheckboxTest.swift
+++ Example/Tests/AndesCheckboxTest.swift
@@ -14,6 +14,48 @@ class AndesCheckboxTests: QuickSpec {
     override func spec() {
         describe("Andes checkbox should draw its view depends on their properties") {
             context("When creating AndesCheckbox") {
+                it("Has the idle view type, right view align, selected view status check align") {
+                    let checkbox = AndesCheckbox(type: .idle, align: .right, status: .selected, title: "Checkbox")
+                    let alignLeft = AndesCheckboxAlign.left
+
+                    checkbox.align = alignLeft
+                    let configuration = AndesCheckboxViewConfig.init(for: checkbox)
+
+                    expect(configuration.align).to(equal(alignLeft))
+
+                }
+
+                it("Has the idle view error, left view align, unselected view status and border color red") {
+                    let checkbox = AndesCheckbox(type: .error, align: .left, status: .unselected, title: "Checkbox")
+                    let errorBorderColor = UIColor.Andes.red500
+
+                    let configuration = AndesCheckboxViewConfig.init(for: checkbox)
+
+                    expect(configuration.borderColor).to(equal(errorBorderColor))
+
+                }
+
+                it("Has the idle view error, left view align, selected view status with no border") {
+                    let checkbox = AndesCheckbox(type: .idle, align: .left, status: .selected, title: "Checkbox")
+
+                    let configuration = AndesCheckboxViewConfig.init(for: checkbox)
+
+                    expect(configuration.borderColor).to(beNil())
+
+                }
+
+                it("Has the idle view error, left view align, selected view status and background color + icon") {
+                    let checkbox = AndesCheckbox(type: .idle, align: .left, status: .selected, title: "Checkbox")
+
+                    let selectedBackgroundColor = UIColor.Andes.blueML500
+
+                    let configuration = AndesCheckboxViewConfig.init(for: checkbox)
+
+                    expect(configuration.backgroundColor).to(equal(selectedBackgroundColor))
+                    expect(configuration.icon).notTo(beNil())
+
+                }
+
                 it("Has the idle view type, left view align, selected view status") {
                     let checkbox = AndesCheckbox(type: .idle, align: .left, status: .selected, title: "Checkbox")
                     expect(checkbox.title).to(equal("Checkbox"))
