4c31ceb84d2d804d268c1b9062c739e6fc0e06b4
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2020-06-29 11:49:35 -0300

    Add class

---
+++ RadioButton/AndesRadioButton.swift
@@ -0,0 +1,13 @@
+//
+//  AndesRadioButton.swift
+//  AndesUI
+//
+//  Created by Rodrigo Pintos Costa on 6/29/20.
+//
+
+import Foundation
+import UIKit
+
+@objc public class AndesRadioButton: UIView {
+
+}
