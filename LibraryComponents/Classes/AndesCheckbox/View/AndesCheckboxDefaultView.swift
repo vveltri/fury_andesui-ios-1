21774111ddaa8995a1ac156d9ce5a9702d6169b3
Samuel Sainz <samuelsainz7@gmail.com>
2020-07-01 15:35:13 -0300

    Extending tappable area

--- LibraryComponents/Classes/AndesCheckbox/View/AndesCheckboxDefaultView.swift
+++ LibraryComponents/Classes/AndesCheckbox/View/AndesCheckboxDefaultView.swift
@@ -8,27 +8,27 @@
 import Foundation
 
 class AndesCheckboxDefaultView: UIView, AndesCheckboxView {
+
     @IBOutlet weak var label: UILabel!
 
     @IBOutlet weak var labelToLeftButtonConstraint: NSLayoutConstraint!
-
     @IBOutlet weak var labelToLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var labelToRightButtonConstraint: NSLayoutConstraint!
     @IBOutlet weak var labelToTrailingConstraint: NSLayoutConstraint!
 
-    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var leftBox: UIImageView!
+    @IBOutlet weak var leftTappableArea: UIButton!
 
-    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightBox: UIImageView!
+    @IBOutlet weak var rightTappableArea: UIButton!
 
     @IBOutlet var checkboxView: UIView!
-    internal var backgroundLayer: CALayer
 
     weak var delegate: AndesCheckboxViewDelegate?
 
     var config: AndesCheckboxViewConfig
     init(withConfig config: AndesCheckboxViewConfig) {
         self.config = config
-        self.backgroundLayer = CALayer()
         super.init(frame: .zero)
         setup()
     }
@@ -52,21 +52,18 @@ class AndesCheckboxDefaultView: UIView, AndesCheckboxView {
 
     override init(frame: CGRect) {
         self.config = AndesCheckboxViewConfig()
-        self.backgroundLayer = CALayer()
         super.init(frame: frame)
         setup()
     }
 
     required init?(coder: NSCoder) {
         self.config = AndesCheckboxViewConfig()
-        self.backgroundLayer = CALayer()
         super.init(coder: coder)
         setup()
 
     }
 
     init() {
-        self.backgroundLayer = CALayer()
         self.config = AndesCheckboxViewConfig()
         super.init(frame: .zero)
         setup()
@@ -77,74 +74,84 @@ class AndesCheckboxDefaultView: UIView, AndesCheckboxView {
         self.addSubview(checkboxView)
         checkboxView.pinToSuperview()
         checkboxView.translatesAutoresizingMaskIntoConstraints = false
+        initialize()
         updateView()
+    }
+
+    func initialize() {
+        self.label.setAndesStyle(style: AndesStyleSheetManager.styleSheet.bodyM(color: config.textColor))
     }
 
     func updateView() {
         clearView()
         self.label.text = config.title
-        let checkboxIcon: String? = config.icon
-        let iconColor: UIColor? = config.iconColor
+        updateBoxesViews()
+        updateBoxesBorders()
+        updateIcons()
+    }
 
-        setButtonView()
-        if let currentIcon = checkboxIcon, !currentIcon.isEmpty {
-            if let currentIconColor = iconColor {
-                if self.leftButton.isHidden == false {
+    func updateIcons() {
+        if let currentIcon = config.icon, !currentIcon.isEmpty {
+            if let currentIconColor = config.iconColor {
+                if self.leftBox.isHidden == false {
                     AndesIconsProvider.loadIcon(name: currentIcon) { checkboxIcon in
-                        self.leftButton.setImage(checkboxIcon, for: .normal)
+                        self.leftBox.image = checkboxIcon
                     }
-                    self.leftButton.tintColor = currentIconColor
+                    self.leftBox.tintColor = currentIconColor
                 } else {
                     AndesIconsProvider.loadIcon(name: currentIcon) { checkboxIcon in
-                        self.rightButton.setImage(checkboxIcon, for: .normal)
+                        self.rightBox.image = checkboxIcon
                     }
-                    self.rightButton.tintColor = currentIconColor
+                    self.rightBox.tintColor = currentIconColor
                 }
             }
         }
-        self.label.setAndesStyle(style: AndesStyleSheetManager.styleSheet.bodyM(color: config.textColor))
-        self.backgroundLayer.cornerRadius = config.cornerRadius
-        if let currentBorderColor = config.borderColor, let currentBorderSize = config.borderSize {
-            if !self.leftButton.isHidden {
-                self.leftButton.layer.borderColor = currentBorderColor.cgColor
-                self.leftButton.layer.borderWidth = currentBorderSize
-            } else {
-                self.rightButton.layer.borderColor = currentBorderColor.cgColor
-                self.rightButton.layer.borderWidth = currentBorderSize
-            }
-        }
-
     }
 
-    func setButtonView() {
+    func updateBoxesViews() {
         if config.align == AndesCheckboxAlign.left {
-            self.rightButton.isHidden = true
-            self.leftButton.isHidden = false
-            self.leftButton.backgroundColor = config.backgroundColor
-            self.backgroundLayer = self.leftButton.layer
+            self.rightBox.isHidden = true
+            self.leftBox.isHidden = false
+            self.rightTappableArea.isHidden = true
+            self.leftTappableArea.isHidden = false
+            self.leftBox.backgroundColor = config.backgroundColor
+            self.leftBox.layer.cornerRadius = config.cornerRadius
             self.labelToTrailingConstraint.priority = .defaultHigh
             self.labelToRightButtonConstraint.priority = .defaultLow
             self.labelToLeftButtonConstraint.priority = .defaultHigh
             self.labelToLeadingConstraint.priority = .defaultLow
-         } else {
-            self.rightButton.isHidden = false
-            self.leftButton.isHidden = true
-            self.rightButton.backgroundColor = config.backgroundColor
-            self.backgroundLayer = self.rightButton.layer
+        } else {
+            self.rightBox.isHidden = false
+            self.leftBox.isHidden = true
+            self.rightTappableArea.isHidden = false
+            self.leftTappableArea.isHidden = true
+            self.rightBox.backgroundColor = config.backgroundColor
+            self.rightBox.layer.cornerRadius = config.cornerRadius
             self.labelToTrailingConstraint.priority = .defaultLow
             self.labelToRightButtonConstraint.priority = .defaultHigh
             self.labelToLeftButtonConstraint.priority = .defaultLow
             self.labelToLeadingConstraint.priority = .defaultHigh
-         }
+        }
+    }
+
+    func updateBoxesBorders() {
+        if let currentBorderColor = config.borderColor, let currentBorderSize = config.borderSize {
+            if !self.leftBox.isHidden {
+                self.leftBox.layer.borderColor = currentBorderColor.cgColor
+                self.leftBox.layer.borderWidth = currentBorderSize
+            } else {
+                self.rightBox.layer.borderColor = currentBorderColor.cgColor
+                self.rightBox.layer.borderWidth = currentBorderSize
+            }
+        }
     }
 
     func clearView() {
-        self.leftButton.layer.borderColor = UIColor.clear.cgColor
-        self.rightButton.layer.borderColor = UIColor.clear.cgColor
-        self.leftButton.layer.borderWidth = 0
-        self.rightButton.layer.borderWidth = 0
-        self.leftButton.setImage(nil, for: .normal)
-        self.rightButton.setImage(nil, for: .normal)
+        self.leftBox.layer.borderColor = UIColor.clear.cgColor
+        self.rightBox.layer.borderColor = UIColor.clear.cgColor
+        self.leftBox.layer.borderWidth = 0
+        self.rightBox.layer.borderWidth = 0
+        self.leftBox.image = nil
+        self.rightBox.image = nil
     }
-
 }
