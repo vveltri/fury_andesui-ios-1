3d589facc99fbf65e9738f549d2cf00dbe75fe0a
Rodrigo Pintos <rodrigo.pintos@mercadolibre.com>
2020-07-02 15:50:44 -0300

    Add testapp func

--- Example/AndesUI/RadioButton/Views/AndesRadioButtonViewController.swift
+++ Example/AndesUI/RadioButton/Views/AndesRadioButtonViewController.swift
@@ -14,18 +14,59 @@ protocol RadioButtonView: NSObject {
 }
 
 class AndesRadioButtonViewController: UIViewController, RadioButtonView {
+    @IBOutlet weak var typeText: UITextField!
+
+    @IBOutlet weak var statusTxt: UITextField!
+    @IBOutlet weak var alignTxt: UITextField!
+    @IBOutlet weak var radioButtonTxt: UITextField!
+
+    @IBOutlet weak var updateBtn: UIButton!
+    @IBOutlet weak var clearBtn: UIButton!
+
+    var typePicker: UIPickerView = UIPickerView()
+    var statusPicker: UIPickerView = UIPickerView()
+    var alignPicker: UIPickerView = UIPickerView()
 
     @IBOutlet weak var radioButton: AndesRadioButton!
 
     override func viewDidLoad() {
 
         radioButton.setRadioButtonTapped(callback: didTapIdle(radiobutton:))
+        createPickerViews()
+    }
 
-        // Idle - Unselected - left
-        /*let idleRadioButtonView: AndesRadioButton! = AndesRadioButton(type: AndesRadioButtonType.idle, align: AndesRadioButtonAlign.left, status: AndesRadioButtonStatus.unselected, title: "Idle - Unselected - left")
-        
-        stackView.addArrangedSubview(idleRadioButtonView)*/
+    @IBAction func updateTapped(_ sender: Any) {
+        self.radioButton.title = radioButtonTxt.text
+    }
 
+    @IBAction func clearTapped(_ sender: Any) {
+        self.radioButton.title = "Andes RadioButton"
+        self.radioButton.type = .idle
+        self.radioButton.status = .unselected
+        self.radioButton.align = .left
+
+        typeText.text = self.radioButton.type.toString()
+        statusTxt.text = self.radioButton.status.toString()
+        alignTxt.text = self.radioButton.align.toString()
+
+        radioButtonTxt.text = ""
+    }
+
+    func createPickerViews() {
+        typeText.inputView = typePicker
+        typePicker.delegate = self
+        typePicker.dataSource = self
+        typeText.text = self.radioButton.type.toString()
+
+        statusTxt.inputView = statusPicker
+        statusPicker.delegate = self
+        statusPicker.dataSource = self
+        statusTxt.text = self.radioButton.status.toString()
+
+        alignTxt.inputView = alignPicker
+        alignPicker.delegate = self
+        alignPicker.dataSource = self
+        alignTxt.text = self.radioButton.align.toString()
     }
 
     func didTapIdle(radiobutton: AndesRadioButton) {
@@ -46,4 +87,61 @@ class AndesRadioButtonViewController: UIViewController, RadioButtonView {
         self.present(alert, animated: true, completion: nil)
     }
 
+}
+
+extension AndesRadioButtonViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == typePicker {
+           typeText.resignFirstResponder()
+            self.radioButton.type = AndesRadioButtonType.init(rawValue: row)!
+            typeText.text = self.radioButton.type.toString()
+        }
+        if pickerView == statusPicker {
+            statusTxt.resignFirstResponder()
+            self.radioButton.status = AndesRadioButtonStatus.init(rawValue: row)!
+            statusTxt.text = self.radioButton.status.toString()
+
+        }
+        if pickerView == alignPicker {
+            alignTxt.resignFirstResponder()
+            self.radioButton.align = AndesRadioButtonAlign.init(rawValue: row)!
+            alignTxt.text = self.radioButton.align.toString()
+        }
+    }
+}
+
+extension AndesRadioButtonViewController: UIPickerViewDataSource {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == typePicker {
+            return 3
+        }
+        if pickerView == statusPicker {
+            return 2
+        }
+        if pickerView == alignPicker {
+            return 2
+        }
+        return 0
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        if pickerView == typePicker {
+            let type = AndesRadioButtonType.init(rawValue: row)!
+            return type.toString()
+        }
+        if pickerView == statusPicker {
+             let status = AndesRadioButtonStatus.init(rawValue: row)!
+            return status.toString()
+        }
+        if pickerView == alignPicker {
+             let align = AndesRadioButtonAlign.init(rawValue: row)!
+            return align.toString()
+        }
+        return ""
+    }
 }
