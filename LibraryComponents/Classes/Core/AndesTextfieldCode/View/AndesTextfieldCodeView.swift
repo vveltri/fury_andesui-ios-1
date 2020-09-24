b888784571088ec8cab72e67011e5053632b3937
Esteban Adrian Boffa <esteban.boffa@mercadolibre.com>
2020-09-24 18:47:22 -0300

    Refactor name

--- LibraryComponents/Classes/Core/AndesTextfieldCode/View/AndesTextfieldCodeView.swift
+++ LibraryComponents/Classes/Core/AndesTextfieldCode/View/AndesTextfieldCodeView.swift
@@ -1,5 +1,5 @@
 //
-//  AndesTextfieldCodeProtocol.swift
+//  AndesTextfieldCodeView.swift
 //  AndesUI
 //
 //  Created by Esteban Adrian Boffa on 16/09/2020.
@@ -10,6 +10,6 @@ import Foundation
 /**
  Internal protocol that specifies the behaviour a view must provide to be a valid representation of an AndesCodeTextField
  */
-protocol AndesTextfieldCodeProtocol: UIView {
+protocol AndesTextfieldCodeView: UIView {
     func update(withConfig config: AndesTextFieldCodeViewConfig)
 }
