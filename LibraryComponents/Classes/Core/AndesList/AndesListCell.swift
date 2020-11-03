bf7768b40f9a4647938b377783e81aa3a9418e21
joalonsopint <jonathandiego.alonsopinto@mercadolibre.com.co>
2020-11-03 13:12:47 -0500

    Refactor internal property

--- LibraryComponents/Classes/Core/AndesList/AndesListCell.swift
+++ LibraryComponents/Classes/Core/AndesList/AndesListCell.swift
@@ -24,33 +24,33 @@ import UIKit
     @IBOutlet weak var paddingTopThumbnailConstraint: NSLayoutConstraint!
     @IBOutlet weak var paddingBottomThumbnailConstraint: NSLayoutConstraint!
 
-    public var title: String = String()
-    public var subTitle: String = String()
-    public var type: AndesCellType?
-    public var fontStyle: AndesFontStyle = AndesFontStyle(textColor: .black,
+    internal var title: String = String()
+    internal var subTitle: String = String()
+    internal var type: AndesCellType?
+    internal var fontStyle: AndesFontStyle = AndesFontStyle(textColor: .black,
                                                           font: UIFont.boldSystemFont(ofSize: 16),
                                                           lineSpacing: 2)
-    public var fontSubTitleStyle: AndesFontStyle = AndesFontStyle(textColor: .black,
+    internal var fontSubTitleStyle: AndesFontStyle = AndesFontStyle(textColor: .black,
                                                                      font: UIFont.boldSystemFont(ofSize: 16),
                                                                      lineSpacing: 2)
-    public var paddingLeftCell: CGFloat = 0
-    public var paddingRightCell: CGFloat = 0
-    public var paddingTopCell: CGFloat = 0
-    public var paddingBottomCell: CGFloat = 0
-    public var subTitleHeight: CGFloat = 0
-    public var separatorHeight: CGFloat = 0
-    public var heightConstraint: CGFloat = 0
-    public var titleHeight: CGFloat = 0
-    public var chevron: String?
-    public var chevronSize: CGFloat? = 0
-    public var thumbnailLeft: UIImage?
-    public var thumbnailSize: CGFloat? = 0
-    public var separatorThumbnailWidth: CGFloat? = 0
-    public var paddingTopThumbnail: CGFloat? = 0
-    public var paddingBottomThumbnail: CGFloat? = 0
-    public var separatorChevronWidth: CGFloat? = 0
-    public var paddingTopChevron: CGFloat? = 0
-    public var paddingBottomChevron: CGFloat? = 0
+    internal var paddingLeftCell: CGFloat = 0
+    internal var paddingRightCell: CGFloat = 0
+    internal var paddingTopCell: CGFloat = 0
+    internal var paddingBottomCell: CGFloat = 0
+    internal var subTitleHeight: CGFloat = 0
+    internal var separatorHeight: CGFloat = 0
+    internal var heightConstraint: CGFloat = 0
+    internal var titleHeight: CGFloat = 0
+    internal var chevron: String?
+    internal var chevronSize: CGFloat? = 0
+    internal var thumbnailLeft: UIImage?
+    internal var thumbnailSize: CGFloat? = 0
+    internal var separatorThumbnailWidth: CGFloat? = 0
+    internal var paddingTopThumbnail: CGFloat? = 0
+    internal var paddingBottomThumbnail: CGFloat? = 0
+    internal var separatorChevronWidth: CGFloat? = 0
+    internal var paddingTopChevron: CGFloat? = 0
+    internal var paddingBottomChevron: CGFloat? = 0
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
