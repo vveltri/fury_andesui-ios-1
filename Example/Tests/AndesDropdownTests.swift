d017356700706ae19c0afccba3117e915350153d
joalonsopint <jonathandiego.alonsopinto@mercadolibre.com.co>
2020-12-01 14:09:14 -0500

    Unit test

---
+++ Example/Tests/AndesDropdownTests.swift
@@ -0,0 +1,15 @@
+//
+//  AndesDropdownTests.swift
+//  AndesUI-demoapp
+//
+//  Created by Jonathan Alonso Pinto on 1/12/20.
+//  Copyright © 2020 MercadoLibre. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import AndesUI
+
+class AndesDropdownTests: QuickSpec {
+
+}
