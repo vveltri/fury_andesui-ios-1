1e129ad41f2fc01e9c7c675f0bd154645b4a565e
joalonsopint <jonathandiego.alonsopinto@mercadolibre.com.co>
2020-12-01 14:13:06 -0500

    Fix percent bottom sheet

--- LibraryComponents/Classes/AndesBottomSheet/AndesBottomSheetViewController.swift
+++ LibraryComponents/Classes/AndesBottomSheet/AndesBottomSheetViewController.swift
@@ -19,7 +19,7 @@ private enum Constants {
             return UIApplication.shared.statusBarFrame.height
         }
     }
-    static let sizes: [AndesBottomSheetSize] = [.min(.intrinsic, .percent(0.80)),
+    static let sizes: [AndesBottomSheetSize] = [.min(.intrinsic, .percent(0.60)),
                                                 .min(.intrinsic, .fixedFromTop(Constants.marginFromTop))]
 }
 
