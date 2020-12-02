012df2db104f4db434510b17066da2689534921b
joalonsopint <jonathandiego.alonsopinto@mercadolibre.com.co>
2020-12-02 11:04:57 -0500

    Fix delegate

--- Example/AndesUI/DropDown/View/DropDownObjcViewController.h
+++ Example/AndesUI/DropDown/View/DropDownObjcViewController.h
@@ -11,7 +11,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface DropDownObjcViewController : UIViewController <AndesDropdownDelegate>
+@interface DropDownObjcViewController : UIViewController
 
 @end
 
--- Example/AndesUI/DropDown/View/DropDownObjcViewController.m
+++ Example/AndesUI/DropDown/View/DropDownObjcViewController.m
@@ -8,7 +8,7 @@
 
 #import "DropDownObjcViewController.h"
 
-@interface DropDownObjcViewController ()
+@interface DropDownObjcViewController () <AndesDropdownDelegate>
 
 @property(weak, nonatomic) IBOutlet AndesDropdown *andesDropdown;
 @property(weak, nonatomic) IBOutlet AndesRadioButton *radioButtonForm;
