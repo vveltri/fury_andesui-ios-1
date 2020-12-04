6c9f112b8a5d85743687b1fa1eaa1b5107e87703
federiconosmor <federico.nosmor@mercadolibre.com>
2020-12-04 12:32:24 -0300

    Feature/Andes Card Body Padding (#172)
    * Sets the body padding of the AndesCard
    
    * Modifico testapp agregando parámetro bodyPadding
    
    * Test default param body padding
    Test constraint
    Test unit set padding and body padding
    
    * Revert line 41 to version COCOAPODS: 1.8.4

---
+++ LibraryComponents/Classes/Core/AndesCard/BodyPadding/AndesCardBodyPaddingProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AndesCardBodyPaddingProtocol.swift
+//  AndesUI
+//
+//  Created by Federico Gabriel Nosmor on 19/11/2020.
+//
+
+import Foundation
+
+internal protocol AndesCardBodyPaddingProtocol {
+    var bodyPaddingSize: UInt { get }
+}
---
+++ LibraryComponents/Classes/Core/AndesCard/BodyPadding/AndesCardBodyPaddingSmall.swift
@@ -0,0 +1,12 @@
+//
+//  AndesCardBodyPaddingSmall.swift
+//  AndesUI
+//
+//  Created by Federico Gabriel Nosmor on 19/11/2020.
+//
+
+import Foundation
+
+struct AndesCardBodyPaddingSmall: AndesCardBodyPaddingProtocol {
+    let bodyPaddingSize: UInt = 16
+}
