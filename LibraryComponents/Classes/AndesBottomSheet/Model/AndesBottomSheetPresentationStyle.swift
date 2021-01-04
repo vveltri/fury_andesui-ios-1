e684fea521b567ff8f927a62597432f906a5f357
Iñaki Yabar <inaki.yabar@mercadolibre.com>
2021-01-04 11:48:44 -0300

    Add description to enum

--- LibraryComponents/Classes/AndesBottomSheet/Model/AndesBottomSheetPresentationStyle.swift
+++ LibraryComponents/Classes/AndesBottomSheet/Model/AndesBottomSheetPresentationStyle.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+
+/// Used to define the way the bottom sheet will be presented
 @objc public enum AndesBottomSheetPresentationStyle: Int, AndesEnumStringConvertible {
-    case fixed
-    case intrinsic
+    case fixed /// default style, maximum initial height is 60% of the screen
+    case intrinsic /// initial height can excede 60% of the screen, depending on the intrinsic content size of the contained controller
 
     public static func keyFor(_ value: AndesBottomSheetPresentationStyle) -> String {
         switch value {
